a9d87a1e108b2256070cd8842f5a9e320f3bf273
193282 <193282@ids.upchiapas.edu.mx>
2022-07-21 19:22:49 -0500

    login y registro actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Info Ruta v1',
       debugShowCheckedModeBanner: false,
-      initialRoute: 'transportista',
+      initialRoute: 'register',
       routes: {
       'map1': (BuildContext context) =>  const FirstPoint(),
       'map2': (BuildContext context) =>  const EndPoint (),
--- lib/service/login_service.dart
+++ lib/service/login_service.dart
@@ -4,7 +4,8 @@ import 'package:http/http.dart' as http;
 Future<Map<String, dynamic>> login(String email, String password) async {
   Map<String, String> headers = {'Content-Type': 'application/json'};
   String loginAPI = '/api/login/';
-  String host = '192.168.100.3:3000';
+  // String host = '192.168.100.3:3000';
+  String host = '10.0.2.2:3001';
 
   try {
     final response = await http.post(
@@ -14,11 +15,18 @@ Future<Map<String, dynamic>> login(String email, String password) async {
         {'email': email, 'password': password},
       ),
     );
+    // if (response.statusCode == 200) {
+    //   final data = json.decode(response.body);
+    //   Map<String, dynamic> responseMap = {
+    //     'status': data['status'],
+    //     'token': data['token']
+    //   };
+    //   return responseMap;
+    // }
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       Map<String, dynamic> responseMap = {
-        'status': data['status'],
-        'token': data['token']
+        'status': data['status']
       };
       return responseMap;
     } else {
